8d83427447aee95645b60e84ee36ca6e4a49bdb9
GitHub Action <action@github.com>
2020-10-16 16:59:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/salmonking72/themesense/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/salmonking72/themesense/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.salmonking72.themesense.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.salmonking72.themesense.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/salmonking72/themesense/services/MyProjectService.kt
+++ src/main/kotlin/com/github/salmonking72/themesense/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.salmonking72.themesense.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.salmonking72.themesense.MyBundle
 
 class MyProjectService(project: Project) {
 
